6a7a15498c01464691ea08400d6e4ae8801aa8b0
irilstr <irils@stud.ntnu.no>
2025-03-10 18:12:30 +0100

    Started making start screen
    Co-authored-by: simenen123 <simenen123@users.noreply.github.com>

---
+++ src/components/startScreen.tsx
@@ -0,0 +1,9 @@
+export default function StartScreen() {
+  return (
+    <>
+      <div className="h-screen bg-[#3CB4E8] flex flex-col items-center justify-center relative">
+        <h1>TicTacToe</h1>
+      </div>
+    </>
+  );
+}
