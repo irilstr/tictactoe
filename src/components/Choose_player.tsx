4a64780cd7973c1f45105bb31ab69cbc79e04277
simenen123 <Simenlog@outlook.com>
2025-03-10 19:08:03 +0100

    fiksa hoved div

---
+++ src/components/Choose_player.tsx
@@ -0,0 +1,10 @@
+export default function Square() {
+    return (
+        <div id="alt" className="h-screen bg-gray-100" >
+            <div id="title" className="text-center mt-6">
+                <h1>Choose your player</h1>
+            </div>
+            <button className="square text-2xl text-blue-500" type="button">X</button>
+        </div>
+    );
+}
